40e8a1ea17a6e40543810bbf3ee0f5d950e21a3a
Justin Liu <JNLiu5@gmail.com>
2016-05-22 18:38:52 -0700

    Implemented functions

--- packet.h
+++ packet.h
@@ -16,18 +16,30 @@ public:
 
 class Packet {
 public:
+	Packet();
+	Packet(TcpHeader h, Segment s);
 	Packet(uint16_t seqNo, uint16_t ackNo, uint16_t rcvWin, uint8_t flags, Segment seg);
+	void setHeader(TcpHeader h);
+	void setSegment(Segment seg);
 	void setSeqNo(uint16_t seqNo);
 	void setAckNo(uint16_t ackNo);
 	void setRcvWin(uint16_t rcvWin);
 	void setFlags(uint8_t flags);
-	void setSegment(Segment seg);
 	TcpHeader getHeader();
 	Segment getSegment();
 private:
 	TcpHeader m_header;
 	Segment m_seg;
 };
+
+Packet::Packet() {
+	//nothing to do
+}
+
+Packet::Packet(TcpHeader h, Segment s) {
+	m_header = h;
+	m_seg = s;
+}
 
 Packet::Packet(uint16_t seqNo, uint16_t ackNo, uint16_t rcvWin, uint8_t flags, Segment seg) {
 	m_header.seqNo = seqNo;
@@ -37,22 +49,37 @@ Packet::Packet(uint16_t seqNo, uint16_t ackNo, uint16_t rcvWin, uint8_t flags, S
 	m_seg = seg;
 }
 
-void Packet::setHeader(TcpHeader header) {
-	m_header = header;
+void Packet::setHeader(TcpHeader h) {
+	m_header = h;
 }
 
 void Packet::setSegment(Segment seg) {
 	m_seg = seg;
 }
 
+void Packet::setSeqNo(uint16_t seqNo) {
+	m_header.seqNo = seqNo;
+}
+
+void Packet::setAckNo(uint16_t ackNo) {
+	m_header.ackNo = ackNo;
+}
+
+void Packet::setRcvWin(uint16_t rcvWin) {
+	m_header.rcvWin = rcvWin;
+}
+
+void Packet::setFlags(uint16_t flags) {
+	m_header.flags = flags;
+}
+
 TcpHeader Packet::getHeader() {
 	return m_header;
 }
 
-Segment Packet::getSegment() {
+TcpHeader Packet::getSegment() {
 	return m_seg;
 }
-
 
 
  #endif
