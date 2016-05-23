2e325bf835f54c15e1aeeb2576ed60a65b48a74e
anderson <AndersonHuang95@gmail.com>
2016-05-23 15:52:19 -0700

    Setting up client connection code

--- client.cpp
+++ client.cpp
@@ -1,14 +1,74 @@
 #include <string>
 #include <thread>
 #include <iostream>
-
+#include <string.h> 	// memset, memcpy 
+#include <sys/socket.h> // socket API 
+#include <netinet/in.h> // sockaddr_in
+#include <netdb.h> 
+#include <stdlib.h> 
 #include "client.h"
 
 using namespace std;
 
-int main()
+/*******************
+struct sockaddr_in {
+	__uint8_t sin_len; 
+	sa_family_t sin_family; 
+	in_port_t sin_port; 
+	struct in_addr sin_addr; 
+	char sin_zero[8]; 
+};
+********************/
+
+void error(char *msg){
+	perror(msg); 
+	exit(1); 
+}
+
+int main(int argc, char **argv)
 {
 	// TODO: Parse command line arguments 
+	if (arg != 3) {
+		cerr < "Usage: " << argv[0] << " <SERVER-HOST-OR-IP>" << " PORT-NUMBER" << endl; 
+		exit(1); 
+	}
+
+	// Decode hostname, assuming it is not already an IP address 
+	// argv[1] = hostname/IP, argv[2] = port #
+	struct sockaddr_in servaddr;
+	memset((char *)&servaddr, 0, sizeof(servaddr)); 
+	servaddr.sin_family = AF_INET; 
+	servaddr.sin_port = htons(argv[2]); 
+
+	// Get host IP address 
+	struct hostent *hp; 
+	hp = gethostbyname(argv[1]); 
+	if (hp)
+		memcpy((void *)&servaddr.sin_addr, hp->h_addr_list[0], hp->h_length); 
+	else 
+		memcpy((void *)&servaddr.sin_addr, argv[1], strlen(argv[1])); 
+
+	// Create client socket
+	int sockfd; 
+	if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) { 
+		perror("socket creation failed"); 
+		exit(1); 
+	}
+
+	// bind to arbitrary return address since no application will initiate communication here
+	// INADDR_ANY lets OS choose specific IP address 
+	struct sockaddr_in myaddr; 
+	memset((char *)&myaddr, 0, sizeof(myaddr)); 
+	myaddr.sin_family = AF_INET; 
+	myaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
+	myaddr.sin_port = htons(0); // Let OS pick a port 
+
+	if (bind(sockfd, (struct sockaddr *)&myaddr, sizeof(myaddr)) < 0) { 
+		perror("bind failed"); 
+		exit(1); 
+	}
+
+	// Try decoding server name as an IP address 
 
 	// TODO: Set up TCP connection 
 	// 1) SYN w/ random sequence no. 
